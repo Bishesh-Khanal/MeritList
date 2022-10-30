0858a1c7f6dfbfced8744ac1c723f4f0f8822cee
Bishesh Khanal <bishesh.khanal25@gmail.com>
2022-10-30 21:56:48 +0545

    Created setSize.cpp
    The file to set the number of students to be tabulated has been created

---
+++ setSize.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using std::cout;
+using std::cin;
+
+int set_size(){
+	int size;
+	cout << "\nEnter the number of Candidates to be evaluated: ";
+	cin >> size;
+	return size;
+}
